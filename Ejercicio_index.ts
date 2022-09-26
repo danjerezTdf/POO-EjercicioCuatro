4d3e1aec1cb91474cf3fdb9ff88414b15312f46f
Your Name <dgj7785@gmail.com>
2022-09-25 22:17:26 -0300

    renameFiles

---
+++ Ejercicio_index.ts
@@ -0,0 +1,9 @@
+import { Libro } from './Ejercicio_class_Libro';
+import { GestorLibros } from './Ejercicio_class_GestorLibros';
+import * as fs from 'fs';
+let readlineSync = require('readline-sync');
+
+
+
+
+
--- index.ts
+++
@@ -1,4 +0,0 @@
-import { Libro } from './class_Libro';
-import { GestorLibros } from './class_GestorLibros';
-let readlineSync = require('readline-sync');
-
